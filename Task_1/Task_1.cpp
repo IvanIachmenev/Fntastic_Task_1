ccf3514be338375354014be4eab217cbd98340eb
IACHMAN <3261@list.ru>
2023-01-20 16:49:44 +1000

    Completed Task 1

---
+++ Task_1/Task_1.cpp
@@ -0,0 +1,44 @@
+﻿#include <string>
+#include <map>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    string text;
+    getline(cin, text);
+    transform(text.begin(), text.end(), text.begin(), tolower);
+    map<char, int> map;
+
+    for (auto i = 0; i < text.length(); i++)
+    {
+        auto it = map.find(text[i]);
+        
+        if (it != map.end()) 
+        {
+            map[it->first]++;
+        }
+        else
+        {
+            map.emplace(text[i], 1);
+        }
+
+    }
+
+    for (auto i = 0; i < text.length(); i++)
+    {
+        if (map[text[i]] > 1)
+        {
+            text[i] = ')';
+        }
+        else
+        {
+            text[i] = '(';
+        }
+    }
+
+    cout << text << endl;
+}
